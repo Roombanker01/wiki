40ba9e3c58c6c9c2f579bd3ee163a6f80a2738bb
cellmigrate <287531770@qq.com>
2023-11-03 19:34:09 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: 'light',
       },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
