e5f01c6fce6f30637f5072ba0fd97513d020619c
klaussun7 <133618972+klaussun7@users.noreply.github.com>
2023-12-13 10:26:38 +0800

    fix

--- sidebars.js
+++ sidebars.js
@@ -354,6 +354,22 @@ const sidebars = {
         },
         {
           type: "category",
+          label: "Smart Plug UK",
+          items: [
+            {
+              type: "doc",
+              id: "automation-device/smart-plug-uk/specification",
+              label: "Specification",
+            },
+            {
+              type: "doc",
+              id: "automation-device/smart-plug-uk/quick-start-guide",
+              label: "Quick Start Guide",
+            },
+          ]
+        },
+        {
+          type: "category",
           label: "Smart Thermostat",
           items: [
             {
